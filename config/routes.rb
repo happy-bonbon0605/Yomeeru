d259a2eba0f28717c53460202fe03f0162f14b40
Takahiro Uchiyama <hochweit728@gmail.com>
2014-11-08 18:22:05 +0900

    add routing login page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+
+  get 'session/new'
+  root to: 'session/new'
+
   get '/auth/:provider/callback' => 'sessions#callback'
   get '/signout' => 'sessions#destroy'
 end
