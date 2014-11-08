06286c73e1b42a1ef6a3806211063a93ec658a61
ta1kt0me <p.wadachi@gmail.com>
2014-11-09 01:33:46 +0900

    complete vote

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -11,11 +11,26 @@ class VotesController < ApplicationController
 
   # message for wife
   def message
-    @vote = Vote.new
-    @vote.voted_user_id = params[:vote][:voted_user_id]
+    @vote = Vote.new(votde_user_id_params)
   end
 
   # create vote and message
   def vote
+    @vote = Vote.new(vote_params)
+    @vote.voting_user_id = current_user.id
+    if @vote.save
+      redirect_to :root, notice: '妻へ捧げる愛のメッセージをありがとうございました。'
+    else
+      render :selct, alert: "妻への愛が足りません。やり直しです。"
+    end
+  end
+
+  private
+  def vote_params
+    params.require(:vote).permit(:voted_user_id, :message)
+  end
+
+  def votde_user_id_params
+    params.require(:vote).permit(:voted_user_id)
   end
 end
