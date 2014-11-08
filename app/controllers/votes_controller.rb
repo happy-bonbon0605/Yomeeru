a9716e7c4397a900ebd0130991bfae547bac9bae
ta1kt0me <p.wadachi@gmail.com>
2014-11-08 20:47:36 +0900

    Viewとcontrollerの定義を追加

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,2 +1,18 @@
 class VotesController < ApplicationController
+  # yochiyochi message
+  def index
+  end
+
+  # vote
+  def select
+    @candidates = Candidate.all
+  end
+
+  # message for wife
+  def message
+  end
+
+  # create vote and message
+  def vote
+  end
 end
