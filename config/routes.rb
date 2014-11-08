294d8b89519844a71ffcacbbe120c5c9d8214d92
Takahiro Uchiyama <hochweit728@gmail.com>
2014-11-08 20:13:52 +0900

    fix typo

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   get 'session/new'
-  root to: 'session/new'
+  root to: 'session#new'
 
   get '/auth/:provider/callback' => 'sessions#callback'
   get '/signout' => 'sessions#destroy'
