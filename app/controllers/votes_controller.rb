8ccfd178b2b10d5c9aad34d4d14f4e835e59e3fa
Takahiro Uchiyama <hochweit728@gmail.com>
2014-11-09 01:44:16 +0900

    add index action

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,6 +1,19 @@
 class VotesController < ApplicationController
   # yochiyochi message
   def index
+    config_file_path = './lib/congratulations.yml'
+
+    begin
+      yaml_data = YAML.load_file(config_file_path)
+    rescue
+      # Configファイルが不正な構文 => 終了する
+      logger.error 'congratulations.yml is invalid'
+      exit
+    end
+
+    @congratulations = yaml_data['congratulations']
+
+    @vote = Vote.last
   end
 
   # vote
