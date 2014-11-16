9a59128863c0729f5277843bd176b911a9210d8e
ta1kt0me <p.wadachi@gmail.com>
2014-11-16 13:40:20 +0900

    first, disable his wife button

--- app/decorators/candidate_decorator.rb
+++ app/decorators/candidate_decorator.rb
@@ -1,6 +1,17 @@
 # coding: utf-8
 module CandidateDecorator
   def button_type
-    uid == '0' ? 'btn-success' : 'btn-default'
+    wife? ? 'btn-success' : 'btn-default'
   end
+
+  def button_disable?
+    wife? ? true : false
+  end
+
+  private
+
+  def wife?
+    uid == '0'
+  end
+
 end
